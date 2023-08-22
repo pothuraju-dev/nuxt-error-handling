53644d1b673f5f4e9f3b1ab54e3d55ca986d32ef
marirs-dev <marirs.dev@gmail.com>
2023-08-23 03:43:09 +0530

    first commit

---
+++ plugins/error.ts
@@ -0,0 +1,22 @@
+import { showError } from '#app'
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook('vue:error', (..._args) => {
+    console.log('vue:error')
+    // if (process.client) {
+    //   console.log(..._args)
+    // }
+  })
+  nuxtApp.hook('app:error', (..._args) => {
+    console.log('app:error')
+    // if (process.client) {
+    //   console.log(..._args)
+    // }
+  })
+  nuxtApp.vueApp.config.errorHandler = (..._args) => {
+    console.log('global error handler')
+    showError('global error handler')
+    // if (process.client) {
+    //   console.log(..._args)
+    // }
+  }
+})
